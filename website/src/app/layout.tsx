1a21ce15eb033b5869df306aec5e384203af470c
Manav Patel <62109295+mpat505@users.noreply.github.com>
2025-03-11 13:05:53 +0000

    template running

---
+++ website/src/app/layout.tsx
@@ -0,0 +1,29 @@
+'use client'
+import * as React from 'react';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/theme';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+import ModeSwitch from '@/components/ModeSwitch';
+import AppTheme from '@/shared-theme/AppTheme';
+import AppAppBar from '@/components/AppAppBar';
+
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript attribute="class" />
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <AppTheme {...props}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline enableColorScheme/>
+            <AppAppBar />
+            {props.children}
+            </AppTheme>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
